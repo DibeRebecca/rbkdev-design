52546e773bf9b3a693e0b6591031d9d00e492fe1
DibeRebecca <rebe.taboukouna@gmail.com>
2021-01-04 20:33:09 +0000

    new commit

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'rbkd&d' );
+define( 'DB_NAME', 'heroku_131bb9b32e9f7b1' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'be3405ea7df4f8' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'dibedibe' );
+define( 'DB_PASSWORD', '4c8e903a' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'us-cdbr-east-02.cleardb.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
